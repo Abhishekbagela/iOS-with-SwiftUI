23d56e5d1717f8c43c73d9bd837c70a0d15a7460
Abhishek-ab@ios-dev <abhibagela2015@gmail.com>
2024-08-26 20:57:12 +0530

    SwiftUI-Combine-TCA added

---
+++ SwiftUI-Combine-TCA/SwiftUI-Combine-TCA/SwiftUI_Combine_TCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Combine_TCAApp.swift
+//  SwiftUI-Combine-TCA
+//
+//  Created by Abhishek Bagela on 26/08/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Combine_TCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
